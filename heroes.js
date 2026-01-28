f6f7dd312386db721d069b371afa9092a7291f39
Dr4rreg <aquablaze76@gmail.com>
2026-01-28 13:18:34 +0800

    Update heroes.js

--- heroes.js
+++ heroes.js
@@ -4,20 +4,5 @@ const heroes = [
   { name: "Valentina", early: 4, late: 4, cc: 4 },
   { name: "Nana", early: 3, late: 3, cc: 5 },
   { name: "Lesley", early: 2, late: 4, cc: 2 }
+  // Add more heroes here as needed
 ];
-
-window.onload = function() {
-  const bluePick = document.getElementById("bluePick");
-  const redPick = document.getElementById("redPick");
-  heroes.forEach(h => {
-    const optionA = document.createElement("option");
-    optionA.text = h.name;
-    optionA.value = h.name;
-    bluePick.add(optionA);
-
-    const optionB = document.createElement("option");
-    optionB.text = h.name;
-    optionB.value = h.name;
-    redPick.add(optionB);
-  });
-};
